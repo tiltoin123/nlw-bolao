63247cac890049febaacf1679eda533a7438d012
Italo Eduardo Santos Souza <italohttp25@gmail.com>
2022-11-08 18:30:41 -0300

    primeiros passos com react

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     })
 
     await fastify.listen({
-        port: 3333, host: '0.0.0.0'
+        port: 3333
     })
 }
 
---
+++ web/components/Tweet.tsx
@@ -0,0 +1,7 @@
+export function Tweet(props){
+   return( <div>
+        <h2>Tweet</h2>
+        <p>{props.text}</p>
+        <button>suamae</button>
+    </div>
+)}
