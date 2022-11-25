70dfa970a1ba2fc0fdb1c8760ad713af6e732ae7
Italo Eduardo Santos Souza <italohttp25@gmail.com>
2022-11-25 02:21:41 -0300

    criação da UI

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,36 +6,47 @@ import iconCheckImg from '../assets/icon-check.svg'
 
 export default function Home() {
   return (
-    <div className='max-w-[1124] h-screen mx-auto grid grid-cols-2 items-center'>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
       <main>
         <Image src={logoImg} alt='NLW Copa'/>
 
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+        <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
 
-        <div>
+        <div className='mt-10 flex items-center gap-2'>
           <Image src={usersAvatarExampleImg} alt=''/>
-          <strong>
-            <span>12.592</span>  pessoas já estão usando
+          <strong className='text-gray-100 text-xl'>
+            <span className='text-ignite-500'>12.592</span>  pessoas já estão usando
           </strong>
         </div>
 
-        <form>
-          <input type="text" required placeholder='Qual o nome do seu bolão?'/>
-          <button type='submit'>Criar meu bolão</button>
-          <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+        <form className='mt-10 flex gap-2'>
+          <input type="text" 
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm ' required
+            placeholder='Qual o nome do seu bolão?'/>
+          <button type='submit'
+           className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'>
+            Criar meu bolão
+            </button>
         </form>
-        <div>
-          <div>
+        <p className='mt-4 text-sm text-gray-300 leading-relaxed'>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+
+        <div className='mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100'>
+          <div className='flex items-center gap-6'>
             <Image src={iconCheckImg} alt='marca de certo'/>
-            <div>
-              <span>+2.034</span>
+            <div className='flex flex-col'>
+              <span >+2.034</span>
               <span>Bolões criados</span>
             </div>
           </div>
-          <div>
+
+          <div className='w-px h-14 bg-gray-600'/>
+
+          <div className='flex items-center gap-6'>
             <Image src={iconCheckImg} alt='marca de certo'/>
-            <span>+192.847</span>
-            <span>Palpites enviados</span>
+            <div className='flex flex-col'>
+              <span>+192.847</span>
+              <span>Palpites enviados</span>
+            </div>
           </div>
         </div>
       </main>
